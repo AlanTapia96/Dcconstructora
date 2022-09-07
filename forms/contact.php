f27b3864fd6b139175739ead764928f8fd60e6f6
Alan <tapia.alan.g@gmail.com>
2022-09-07 11:43:52 -0300

    development

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,23 @@
-<?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+<?php 
+	$nombre = $_POST['name'];
+	$email = $_POST['email'];
+	$asunto = 'Formulario Web - FundacionEcoreciclaje.org.ar';
+	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['message'];
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    $cabeceras = 'From: ecoreciclaje@example.com' . "\r\n" .
+    'Reply-To: ecoreciclaje@example.com' . "\r\n" .
+    "Content-type: text/html; charset=UTF-8" . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+    if( mail('tapia.alan.g@gmail.com', $asunto, $mensaje, $cabeceras) ) {
+        echo json_encode(array(
+           'success'=> true,
+           'message' => 'Message sent successfully'
+        ));
+     }else {
+        echo json_encode(array(
+           'error'=> true,
+           'message' => 'Error sending message'
+        ));
+     }
+ ?>
